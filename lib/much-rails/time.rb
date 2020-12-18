8d07ea4395ae118f9546ae3e8a13f73cbd198a36
neilweathersbee <neil.weathersbee.work@gmail.com>
2020-12-18 13:00:57 -0600

    Add MuchRails::Date/Time
    This adds `MuchRails::Date` and `MuchRails::Time` to handle
    `Date` and `Time` conversions.

---
+++ lib/much-rails/time.rb
@@ -0,0 +1,25 @@
+require "much-rails"
+
+module MuchRails; end
+module MuchRails::Time
+  InvalidError = Class.new(TypeError)
+
+  # @example
+  #   MuchRails::Time(nil) # => nil
+  #   MuchRails::Time(" ") # => nil
+  #   MuchRails::Time(Time.current) # => Time
+  #   MuchRails::Time(DateTime.current) # => Time
+  #   MuchRails::Time(Date.zone.today) # => Time
+  #   MuchRails::Time("2020-07-04T08:15:00Z") # => Time
+  def self.for(value)
+    return if value.blank?
+
+    if value.respond_to?(:to_time) && !value.is_a?(::String)
+      value.to_time.utc
+    else
+      ::Time.iso8601(value.to_s).utc
+    end
+  rescue
+    raise MuchRails::Time::InvalidError, "Invalid Time: #{value.inspect}."
+  end
+end
---
+++ test/unit/lib/much-rails/time_tests.rb
@@ -0,0 +1,50 @@
+require "assert"
+
+require "much-rails/time"
+
+module MuchRails::Time
+  class UnitTests < Assert::Context
+    desc "MuchRails::Time"
+    subject { unit_class }
+
+    let(:unit_class) { MuchRails::Time }
+
+    let(:time) { Time.current }
+    let(:utc_time) { time.utc }
+
+    should have_imeths :for
+
+    should "know how to convert time-like representations to Time" do
+      # nil, blank value(s)
+      assert_that(subject.for(nil)).is_nil
+      ["", " "].each do |object|
+        assert_that(subject.for(object)).is_nil
+      end
+
+      # Time, DateTime, or Date
+      objects = [Time.current, DateTime.current, Date.today]
+      objects.each do |object|
+        assert_that(subject.for(object)).equals(object.to_time)
+      end
+
+      # U.S.-formatted String
+      result = subject.for(time.iso8601)
+
+      assert_that(result).is_instance_of(Time)
+      assert_that(result.utc?).is_true
+      assert_that(result.year).equals(utc_time.year)
+      assert_that(result.month).equals(utc_time.month)
+      assert_that(result.day).equals(utc_time.day)
+      assert_that(result.hour).equals(utc_time.hour)
+      assert_that(result.min).equals(utc_time.min)
+      assert_that(result.sec).equals(utc_time.sec)
+
+      # invalid values
+      invalid_objects = ["TEST_VALUE", 42, Class.new]
+      invalid_objects.each do |object|
+        assert_that(-> { subject.for(object) })
+          .raises(MuchRails::Time::InvalidError)
+      end
+    end
+  end
+end

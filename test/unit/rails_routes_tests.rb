e5488bb077332dcf31b78f80e9782e8cc142ee6f
Kelly Redding <kelly@kellyredding.com>
2020-12-31 16:26:11 -0600

    add MuchRails::RailsRoutes
    This is a convenience Singleton to access Rails' url helpers.
    This will be used to generate paths/URLs from the Action router
    which is coming.
    
    Note: I had to bring in Rails as a development dependency so I
    could test this. I did the bare minimum to configure it for
    testing in the test helper.

---
+++ test/unit/rails_routes_tests.rb
@@ -0,0 +1,26 @@
+require "assert"
+require "much-rails/rails_routes"
+
+class MuchRails::RailsRoutes
+  class UnitTests < Assert::Context
+    desc "MuchRails::RailsRoutes"
+    subject { unit_class }
+
+    let(:unit_class) { MuchRails::RailsRoutes }
+
+    should "include Singleton, Rails url helpers" do
+      assert_that(subject).includes(Singleton)
+      assert_that(subject).includes(::Rails.application.routes.url_helpers)
+    end
+  end
+
+  class InitTests < UnitTests
+    desc "when init"
+    subject { unit_class.instance }
+
+    should "know its default URL options" do
+      assert_that(subject.__send__(:default_url_options))
+        .equals(::Rails.application.config.action_mailer.default_url_options)
+    end
+  end
+end

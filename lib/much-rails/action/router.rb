e21cfdaa7b6b4f1bb24a114dd46941e8e5a78360
Kelly Redding <kelly@kellyredding.com>
2020-12-31 16:41:17 -0600

    (hotfix) add basic Router w/ controller constants
    This was missed when I added MuchRails::Action::Controller. It
    needs these constants and I forgot to add them in that previous
    effort.

---
+++ lib/much-rails/action/router.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module MuchRails; end
+module MuchRails::Action; end
+class MuchRails::Action::Router
+  DEFAULT_CONTROLLER_NAME = "application"
+  CONTROLLER_METHOD_NAME = :much_rails_call_action
+  ACTION_CLASS_PARAM_NAME = :much_rails_action_class_name
+end
